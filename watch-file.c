5e83819f625b1c906db8844e9997cb92ac895fb5
Makkes <mail@makk.es>
2012-04-27 11:42:09 +0200

    replaced tabs with spaces

--- watch-file.c
+++ watch-file.c
@@ -8,8 +8,8 @@
 short file_exists(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (file) {
-	fclose(file);
-	return 1;
+        fclose(file);
+        return 1;
     }
     return 0;
 }
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
 
     fd = inotify_init();
     if (fd < 0) {
-	perror("inotify_init");
+        perror("inotify_init");
     }
 
     if(!file_exists(argv[1])) {
@@ -34,48 +34,46 @@ int main(int argc, char *argv[]) {
     printf("Start watching '%s'\n", argv[1]);
 
     while (1) {
-	if (add_watch) {
+        if (add_watch) {
             if(!file_exists(argv[1])) {
                 fprintf(stderr, "File doesn't exist, exiting\n");
                 return 1;
             }
-	    wd = inotify_add_watch(fd, argv[1],
-				   IN_OPEN | IN_CLOSE_WRITE |
-				   IN_CLOSE_NOWRITE | IN_ACCESS | IN_MODIFY
-				   | IN_CREATE | IN_DELETE);
-	    add_watch = 0;
-	}
+            wd = inotify_add_watch(fd, argv[1],
+                    IN_OPEN | IN_CLOSE_WRITE | IN_CLOSE_NOWRITE | IN_ACCESS | IN_MODIFY | IN_CREATE | IN_DELETE);
+            add_watch = 0;
+        }
 
-	i = 0;
-	length = read(fd, buffer, BUFLEN);
-	if (length < 0) {
-	    perror("read");
-	}
+        i = 0;
+        length = read(fd, buffer, BUFLEN);
+        if (length < 0) {
+            perror("read");
+        }
 
-	while (i < length) {
-	    struct inotify_event *event =
-		(struct inotify_event *) &buffer[i];
-	    if (event->mask & IN_CREATE) {
-		printf("File was created\n");
-	    } else if (event->mask & IN_DELETE) {
-		printf("File was deleted\n");
-	    } else if (event->mask & IN_MODIFY) {
-		printf("File was modified\n");
-	    } else if (event->mask & IN_ACCESS) {
-		printf("File was accessed\n");
-	    } else if (event->mask & IN_OPEN) {
-		printf("File was opened\n");
-	    } else if (event->mask & IN_CLOSE_WRITE
-		       || event->mask & IN_CLOSE_NOWRITE) {
-		printf("File was closed\n");
-	    } else if (event->mask & IN_IGNORED) {
-		printf("File was ignored, retrying watch\n");
-		add_watch = 1;
-	    } else {
-		printf("Unknown event: %d", event->mask);
-	    }
-	    i += EVENT_SIZE + event->len;
-	}
+        while (i < length) {
+            struct inotify_event *event =
+                (struct inotify_event *) &buffer[i];
+            if (event->mask & IN_CREATE) {
+                printf("File was created\n");
+            } else if (event->mask & IN_DELETE) {
+                printf("File was deleted\n");
+            } else if (event->mask & IN_MODIFY) {
+                printf("File was modified\n");
+            } else if (event->mask & IN_ACCESS) {
+                printf("File was accessed\n");
+            } else if (event->mask & IN_OPEN) {
+                printf("File was opened\n");
+            } else if (event->mask & IN_CLOSE_WRITE
+                       || event->mask & IN_CLOSE_NOWRITE) {
+                printf("File was closed\n");
+            } else if (event->mask & IN_IGNORED) {
+                printf("File was ignored, retrying watch\n");
+                add_watch = 1;
+            } else {
+                printf("Unknown event: %d", event->mask);
+            }
+            i += EVENT_SIZE + event->len;
+        }
     }
     inotify_rm_watch(fd, wd);
     close(fd);
