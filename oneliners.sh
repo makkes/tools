5b766e72af90f4e12cfcd2fe067ca4cb943c66f6
Max Jonas Werner <mail@makk.es>
2019-06-13 09:15:35 +0200

    Added script collecting bash one-liners

---
+++ oneliners.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script is meant as a collection of one-liners that I use on a regular basis. Just symlink it using one of the following names for the symlink
+# to get the approriate functionality:
+#
+# open-files-per-command: lists the number of open files per command that's currently running.
+
+set -euo pipefail
+
+CMD=$(basename ${0-})
+
+case $CMD in
+    open-files-per-command)
+        IFS=$'\n'; for i in $(ps -A -o pid= -o command=|sed 's/^ *//') ; do printf '%10s %s\n' $(sudo lsof -p $(echo $i | cut -d" " -f1)|tail -n+2|wc -l) $(echo $i | cut -d" " -f2-) ; done
+        ;;
+    *)
+        echo "Unknown command $CMD"
+        exit 1
+        ;;
+esac
