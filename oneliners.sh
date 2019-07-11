29ee62059e37138b99ca133c4fcb1b181c34a90e
Max Jonas Werner <mail@makk.es>
2019-07-11 16:47:04 +0200

    Added wait-for-docker oneliner

--- oneliners.sh
+++ oneliners.sh
@@ -13,6 +13,9 @@ case $CMD in
     open-files-per-command)
         IFS=$'\n'; for i in $(ps -A -o pid= -o command=|sed 's/^ *//') ; do printf '%10s %s\n' $(sudo lsof -p $(echo $i | cut -d" " -f1)|tail -n+2|wc -l) $(echo $i | cut -d" " -f2-) ; done
         ;;
+    wait-for-docker)
+        while ( true ) ; do docker ps && afplay ~/Desktop/cabin_chime.mp3 && break || sleep 1 ; done
+        ;;
     *)
         echo "Unknown command $CMD"
         exit 1
