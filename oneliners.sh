1d9e92d63cd4897645529cc40e72dbd9e09efa60
Max Jonas Werner <mail@makk.es>
2020-07-29 15:41:19 +0200

    chore: simplify some one-liners

--- oneliners.sh
+++ oneliners.sh
@@ -17,10 +17,10 @@ case $CMD in
         IFS=$'\n'; for i in $(ps -A -o pid= -o command=|sed 's/^ *//') ; do printf '%10s %s\n' $(sudo lsof -p $(echo $i | cut -d" " -f1)|tail -n+2|wc -l) $(echo $i | cut -d" " -f2-) ; done
         ;;
     wait-for-docker)
-        while ( true ) ; do docker ps && afplay ~/Desktop/cabin_chime.mp3 && break || sleep 1 ; done
+        while ! docker ps ; do sleep 1 ; done && afplay ~/Desktop/cabin_chime.mp3
         ;;
     monitor-connectivity)
-        while ( true ) ; do ping -c1 google.com && unset FAILURE || if [[ ! ${FAILURE-} ]] ; then afplay ~/Desktop/cabin_chime.mp3; FAILURE=1; fi; sleep 1 ; done
+        while ! ping -c1 google.com ; do sleep 1 ; done && afplay ~/Desktop/cabin_chime.mp3
         ;;
     git-prune-branches)
         git fetch -tpP && git branch -vv|grep ': gone]'|grep -v '^\* '|awk '{print $1}'|xargs -r git branch -D
