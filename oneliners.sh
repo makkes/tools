e2d293463379a9257a6a5af58a19b6e955f97265
Max Jonas Werner <mwerner@d2iq.com>
2021-02-09 13:32:47 +0100

    made oneliners script a little more accessible

--- oneliners.sh
+++ oneliners.sh
@@ -10,26 +10,27 @@
 
 set -euo pipefail
 
-CMD=$(basename ${0-})
+say=
+if command -v say > /dev/null; then
+    say=say
+elif command -v espeak-ng > /dev/null ; then
+    say=espeak-ng
+fi
 
-case $CMD in
-    open-files-per-command)
-        IFS=$'\n'; for i in $(ps -A -o pid= -o command=|sed 's/^ *//') ; do printf '%10s %s\n' $(sudo lsof -p $(echo $i | cut -d" " -f1)|tail -n+2|wc -l) $(echo $i | cut -d" " -f2-) ; done
-        ;;
-    wait-for-docker)
-        while ! docker ps ; do sleep 1 ; done && afplay ~/Desktop/cabin_chime.mp3
-        ;;
-    monitor-connectivity)
-        while ! ping -c1 google.com ; do sleep 1 ; done && afplay ~/Desktop/cabin_chime.mp3
-        ;;
-    git-prune-branches)
-        git fetch -tpP && git branch -vv|grep ': gone]'|grep -v '^\* '|awk '{print $1}'|xargs -r git branch -D
-        ;;
-    kind-with-metallb)
-        kind create cluster && helm upgrade --install -n metallb --create-namespace --set configInline.address-pools[0].name=default --set configInline.address-pools[0].protocol=layer2 --set configInline.address-pools[0].addresses={"172.18.255.1-172.18.255.250"} metallb bitnami/metallb && helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace
-        ;;
-    *)
-        echo "Unknown command $CMD"
-        exit 1
-        ;;
-esac
+declare -A cmds
+cmds[wait-for-docker]="while ! docker ps ; do sleep 1 ; done && ${say} 'docker is running'"
+cmds[open-files-per-command]='IFS=$'"'\n'"'; for i in $(ps -A -o pid= -o command=|sed "s/^ *//") ; do printf "%10s %s\n" $(sudo lsof -p $(echo $i | cut -d" " -f1)|tail -n+2|wc -l) $(echo $i | cut -d" " -f2-) ; done'
+cmds[monitor-connectivity]='while ! ping -c1 google.com ; do sleep 1 ; done && '${say}" 'connectivity established'"
+cmds[git-prune-branches]='git fetch -tpP && git branch -vv|grep ": gone]"|grep -v "^\* "|awk "{print \$1}"|xargs -r git branch -D'
+cmds[kind-with-metallb]='kind create cluster && helm repo add bitnami https://charts.bitnami.com/bitnami && helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && helm repo up && helm upgrade --install -n metallb --create-namespace --set configInline.address-pools[0].name=default --set configInline.address-pools[0].protocol=layer2 --set configInline.address-pools[0].addresses={"172.18.255.1-172.18.255.250"} metallb bitnami/metallb && helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx -n ingress-nginx --create-namespace'
+
+function usage() {
+    echo -e "These are the commands I know:\n"
+    for cmd in "${!cmds[@]}" ; do
+        echo "$cmd"
+    done
+    exit 1
+}
+
+CMD=$(basename "${0-}")
+eval "${cmds[$CMD]:-usage}"
