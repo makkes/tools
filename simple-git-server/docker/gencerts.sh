c8bbe353ad36d218f5b3e439592f28cdad1f8bb8
Max Jonas Werner <mwerner@d2iq.com>
2021-07-30 16:56:50 +0200

    gitserver: use HTTPS
    When building the git server's Docker image a self-signed certificate
    is created on-the-fly and the repo is served through HTTPS now.

---
+++ simple-git-server/docker/gencerts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout privkey.pem \
+    -new \
+    -out cert.pem \
+    -subj /CN=git.default.svc \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat /etc/ssl//openssl.cnf \
+        <(printf '[SAN]\nsubjectAltName=DNS:git.default.svc')) \
+    -sha256 \
+    -days 3650
