b6a89e46d0ca8d090a836a7d9898e448b62764f9
Max Jonas Werner <max@e13.dev>
2022-07-12 10:02:33 +0200

    add backup script

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BKPDIR=/media/max/makkes/$(hostname)
+if [[ -z $(findmnt --target "$BKPDIR") ]] ; then
+    echo "Backup drive not mounted, ${BKPDIR} not found; exiting"
+    exit
+fi
+
+BASEDIR=$(dirname "${BASH_SOURCE[0]}")
+LOGDIR="$HOME/backups/"
+[ -d "$LOGDIR" ] || mkdir -p "$LOGDIR"
+
+DATESTR=$(date +"%Y-%m-%d_%H:%M:%S")
+LOGFILE=$LOGDIR/bkp-$DATESTR.log
+ERRORLOGFILE=$LOGDIR/bkp-$DATESTR-error.log
+
+rsync --exclude-from="$BASEDIR/backup.excl" -vaxAX --delete --ignore-errors / "$BKPDIR" 2> "$ERRORLOGFILE" | tee "$LOGFILE"
