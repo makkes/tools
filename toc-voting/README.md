a1d9cdfc31f411c729694ba0aa5986725bdfb4ce
Max Jonas Werner <max@e13.dev>
2022-11-21 11:53:52 +0100

    add TOC vote monitoring tool

---
+++ toc-voting/README.md
@@ -0,0 +1,9 @@
+# CNCF TOC Vote Monitoring
+
+This is a little python program that fetches responses to a call for votes from the [https://lists.cncf.io/g/cncf-toc/topics](CNCF TOC mailing list) and outputs the results to an HTML page.
+
+It is pretty hard-coded to the original use case of monitoring the [Flux graduation call for votes](https://lists.cncf.io/g/cncf-toc/topic/vote_flux_for_graduation/95047098) but could be adapted easily to watch other topics. Also the list of TOC members is hard-coded.
+
+## Running
+
+Set the two environment variables `LISTS_CNCF_IO_USER` and `LISTS_CNCF_IO_PASSWORD` and run the script. It will then output the vote results in the `index.html` file. Optionally you may set the `OUTPUT` environment variable to a directory that the `index.html` file shall be placed in.
---
+++ toc-voting/toc-voting.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import http.cookiejar
+import urllib.request
+import urllib.parse
+import urllib.error
+import json
+from string import Template
+from datetime import datetime, timezone
+
+
+def req_all(opener, url):
+    try:
+        with opener.open(url) as resp:
+            topic = json.loads(resp.read())
+            res = topic["data"]
+            purl = urllib.parse.urlparse(url)
+            qs = urllib.parse.parse_qs(purl.query)
+            if topic["has_more"]:
+                qs["page_token"] = topic["next_page_token"]
+                lpurl = list(purl)
+                lpurl[4] = urllib.parse.urlencode(qs, doseq=True)
+                res = res + req_all(opener, urllib.parse.urlunparse(lpurl))
+            return res
+    except urllib.error.HTTPError as e:
+        print("HTTP error for {}: status {}".format(url, e.code))
+        print("{}".format(e.read()))
+        sys.exit(1)
+
+
+cj = http.cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+login_values = {
+    "email": os.environ["LISTS_CNCF_IO_USER"],
+    "password": os.environ["LISTS_CNCF_IO_PASSWORD"],
+}
+login_values = urllib.parse.urlencode(login_values).encode("utf-8")
+try:
+    opener.open("https://lists.cncf.io/api/v1/login", login_values)
+except urllib.error.HTTPError as e:
+    print("HTTP error: status {}".format(e.code))
+    print("{}".format(e.read()))
+    sys.exit(1)
+
+
+topics = req_all(
+    opener, "https://lists.cncf.io/api/v1/gettopic?topic_id=95047098&limit=10")
+votes = 0
+voters = []
+toc = [
+    "Davanum Srinivas",
+    "Erin Boyd",
+    "Dave Zolotusky",
+    "Zhang, Cathy H",
+    "Lei Zhang",
+    "Justin Cormack",
+    "Ricardo Rocha",
+    "Emily Fox",
+    "Matt Farina",
+    "Richard Hartmann",
+    "Katie Gamanji",
+]
+for topic in topics:
+    snippet = topic["snippet"]
+    if topic["name"] in toc or "+1 binding" in snippet or "+1B" in snippet or "+1b" in snippet or "+1 b" in snippet:
+        votes += 1
+        voters.append(topic["name"])
+        print("+1 from {}".format(topic["name"]))
+
+with open("toc-voting.html.tmpl") as f:
+    tmpl_content = f.read()
+lastupdate = datetime.now(timezone.utc).strftime("%a %b %d %Y %X %Z")
+tmpl = Template(tmpl_content)
+html = tmpl.substitute(yesno="yes" if votes >
+                       8 else "no ({}/{} necessary votes so far)".format(votes, 8),
+                       lastupdate=lastupdate,
+                       voters="<br/>".join(voters))
+try:
+    outdir = os.environ["OUTDIR"]
+except KeyError:
+    outdir = "."
+with open(os.path.join(outdir, "index.html"), "w") as f:
+    f.write(html)
