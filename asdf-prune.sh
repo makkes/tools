5c564e4de8fbcf169101579f7856ad05acccaeff
Max Jonas Werner <mail@makk.es>
2023-09-28 14:48:20 +0200

    Add command to free up disk space used by asdf packages

---
+++ asdf-prune.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+function usage() {
+    echo "Usage: $0 [OPTION]..."
+    echo "Prune asdf storage to free up disk space".
+    echo
+    echo "  -h, --help          display this help and exit"
+    echo "  -d, --dry-run       don't free up any space but print what would be done"
+}
+
+
+if ! OPTS=$(getopt -o 'hd' --long 'help,dry-run' -- "$@"); then
+    usage
+    exit 1
+fi
+eval set -- "$OPTS"
+unset OPTS
+
+DRY_RUN=
+
+while true ; do
+    case "$1" in
+        '-h'|'--help')
+            usage
+            exit 0
+            ;;
+        '-d'|'--dry-run')
+            DRY_RUN=1
+            shift
+            continue
+            ;;
+        '--')
+            shift
+            break
+            ;;
+        *)
+            echo 'Internal error!' >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -n "$DRY_RUN" ] ; then
+    echo dry run...
+fi
+
+function prune() {
+    local PKG="$1"
+    local VERSION="$2"
+    
+    echo -n "pruning $PKG $VERSION"
+    if [ -n "$DRY_RUN" ] ; then
+        echo " (dry-run)"
+    else
+        echo
+        asdf uninstall "$PKG" "$VERSION"
+    fi
+}
+
+PKG=
+VERSION=
+while IFS= read -r line ; do
+    if [[ "$line" =~ ^[[:space:]][[:space:]] ]] ; then
+        if [ -n "$VERSION" ] ; then
+            prune "$PKG" "$VERSION"
+        fi
+        VERSION="${line##  }"
+    else
+        PKG="$line"
+        VERSION=
+    fi
+done < <(asdf list)
